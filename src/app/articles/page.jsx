7649ce63f7673a3c62c68bf4a683dbbfd38364e4
MelsovEZ <ernurmelsov@gmail.com>
2024-06-06 00:37:46 +0500

    Trying to fix pipeline

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 import articlesData from '@/app/articles/[id]/data.json';
 
+export function generateStaticParams() {
+    return articlesData.map(article => ({
+        params: { id: article.id.toString() }
+    }));
+}
+
 export default function Articles() {
     return (
         <div>
@@ -11,11 +17,11 @@ export default function Articles() {
                         {articlesData.map(article => (
                             <div key={article.id} className="block bg-white shadow-md rounded-lg overflow-hidden w-[80%]">
                                 <Link href={`/articles/${article.id}`}>
-                                        <div className="p-6">
-                                            <h2 className="text-2xl font-semibold mb-4">{article.title}</h2>
-                                            <p className="text-gray-700 mb-4">{article.description}</p>
-                                            <p className="text-blue-600 hover:text-blue-800">Read more</p>
-                                        </div>
+                                    <div className="p-6">
+                                        <h2 className="text-2xl font-semibold mb-4">{article.title}</h2>
+                                        <p className="text-gray-700 mb-4">{article.description}</p>
+                                        <p className="text-blue-600 hover:text-blue-800">Read more</p>
+                                    </div>
                                 </Link>
                             </div>
                         ))}
